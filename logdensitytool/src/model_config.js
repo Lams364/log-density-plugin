e93c9cb1ee25b1e327308e08f35f1e9d25de80c4
Philippe Beaufort <philippe.beaufort.1@ens.etsmtl.ca>
2024-12-08 06:23:24 -0500

    change small log

--- logdensitytool/src/model_config.js
+++ logdensitytool/src/model_config.js
@@ -15,7 +15,7 @@ let configuration = {
     prompt_file : "generate_log.txt", // From prompt Folder
     default_model : "llama3.2:3b",
     default_token : "", // Only used for huggingface
-    response_id :  JsonResponse.responseId, // standard or regex available
+    response_id :  JsonResponse.responseId, // select valid response id
     attributes_to_comment : ["reason"], // list of attributes to comment
     comment_string: "//", // Comment string to add
     injection_variable: "{vscode_content}" // variable to inject vscode content in prompt
